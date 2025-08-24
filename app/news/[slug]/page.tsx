39aca93172395c6440cd1c1eff04d392f97902a2
rzlmiooo <zrrizall10@example.com>
2025-08-24 20:20:49 +0700

    wowo

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -2,7 +2,13 @@ import { Suspense } from "react";
 import NewsDetail from "@/app/suspense/news/slug/news-slug-suspense";
 import LoadingSpinner from "../../components/LoadingSpinner";
 
-export default async function NewsDetailPage({ params }: { params: { slug: string } }) {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function NewsDetailPage({ params }: PageProps ) {
   return (
     <Suspense fallback={<LoadingSpinner loadingText="Loading detail berita..." />}>
       <NewsDetail params={params} />
